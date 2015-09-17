09b512e371fff1ca2615c9eef604b886c60fbc6f
Casey <casey@localhost>
2015-09-17 00:20:57 -0700

    Added Students objects and formatting

--- index.php
+++ index.php
@@ -11,7 +11,45 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Hello out there ... I live";
+        include('student.php');
+            
+        $students = array();
+        
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        
+        $third = new Student();
+        $third->surname = "Hammond";
+        $third->first_name = "Casey";
+        $third->add_email('work','chammond11@my.bcit.ca');
+        $third->add_grade(90);
+        $third->add_grade(80);
+        $third->add_grade(70);
+        $students['c789'] = $third;
+        
+        //Sorts the Students alphabetically by first name
+        ksort($students);
+            
+        foreach($students as $student)
+            echo $student->toString();
         ?>
     </body>
 </html>
